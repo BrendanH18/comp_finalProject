f9ed38596fa7f7a5f959dc629b91c21126603542
Brendan H <151875764+BrendanH18@users.noreply.github.com>
2026-04-03 20:23:30 -0700

    Revert "Completed new methods for Course"
    This reverts commit 30b04d43cc77ad46c79fbc6bad5dd6e7350e32af.

--- src/Course.java
+++ src/Course.java
@@ -31,28 +31,11 @@ public class Course {
     }
 
     public boolean idFull(){
-        if (this.maxCapacity > this.enrolledStudents.size()){
-            return false;
-        }
-        else   {
-            return true;
-        }
+        return this.enrolledStudents.size() >= this.maxCapacity;
     }
 
     public boolean addStudent(String id, RegistrationSystem system){
-        if (this.idFull()){
-            return false;
-        }
 
-        try {
-            StudentIFace studentAdd = system.findStudent(id);
-
-            this.enrolledStudents.add(studentAdd);
-
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
     }
 
     public boolean removeStudent(String id, RegistrationSystem system){
