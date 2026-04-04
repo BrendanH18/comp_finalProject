2c0cd063212ac25ee3eb22ee6049992001c26424
Brendan H <151875764+BrendanH18@users.noreply.github.com>
2026-04-03 20:23:18 -0700

    Revert "More changes to file manager"
    This reverts commit 1a61d5885a4f29f0cc767713bd0654a236de61f2.

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,7 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class FileManager {
 
@@ -14,41 +11,12 @@ public class FileManager {
     }
 
     public ArrayList<Course> loadCourses(String file) {
-        ArrayList<Course> courselist = new ArrayList<>();
-
-        File inputFile = new File(this.dir + "/" + file);
-
-        try {
-            Scanner input = new Scanner(inputFile);
-            while (input.hasNextLine()) {
-                String line = input.nextLine();
-                String[] data = line.split(",");
-                Course nextCourse = new Course(data[1], data[0], data[2]);
-                courselist.add(nextCourse);
-            } return  courselist;
-        }  catch (FileNotFoundException e) {
-            System.out.println("File Not Found");
-            return courselist;
-        }
+        return new ArrayList<>();
     }
 
 
     public ArrayList<StudentIFace> loadStudents(String file) {
-        ArrayList<StudentIFace> resultList = new ArrayList<StudentIFace>();
-        File inputFile = new File(this.dir + "/" + file);
-
-        try {
-            Scanner input = new Scanner(inputFile);
-            while (input.hasNextLine()) {
-                String line = input.nextLine();
-                String[] data = line.split(",");
-                Student nextStudent = new Student(data[1], data[0]);
-                resultList.add(nextStudent);
-            } return  resultList;
-        }  catch (FileNotFoundException e) {
-            System.out.println("File Not Found");
-            return resultList;
-        }
+        return new ArrayList<>();
     }
 
     public boolean saveCourses(String file, ArrayList<Course> courses){
@@ -56,7 +24,7 @@ public class FileManager {
     }
 
     public boolean saveStudents(String file, ArrayList<StudentIFace> students){
-
+        return false;
     }
 
 }
